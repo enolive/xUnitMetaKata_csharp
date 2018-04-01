87f2894fa4ce62b0f5e50e2ce4e626484fff2c55
Christoph Welcz <christoph@welcz.de>
2018-04-01 14:05:12 +0200

    first cleanup in game

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RockPaperScissors
+﻿namespace RockPaperScissors
 {
     public class Game
     {
-        private int _player1Score = 0;
-        private int _player2Score = 0;
-        private IGameListener _listener;
+        private int player1Score;
+        private int player2Score;
+        private readonly IGameListener listener;
 
         public Game(IGameListener listener)
         {
-            _listener = listener;
+            this.listener = listener;
         }
 
         public void PlayRound(string player1, string player2)
         {
             int result = new Round().Play(player1, player2);
-            if (result == 1) _player1Score++;
-            if (result == 2) _player2Score++;
+            if (result == 1) player1Score++;
+            if (result == 2) player2Score++;
 
-            if (_player1Score == 2)
+            if (player1Score == 2)
             {
-                _listener.GameOver(1);
+                listener.GameOver(1);
             }
 
-            if (_player2Score == 2)
+            if (player2Score == 2)
             {
-                _listener.GameOver(2);
+                listener.GameOver(2);
             }
         }
     }
