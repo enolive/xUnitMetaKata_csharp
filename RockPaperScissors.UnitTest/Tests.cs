d8a1a127e3362e612b25f5f21f0b3fa44013259c
Christoph Welcz <christoph@welcz.de>
2018-04-01 13:27:40 +0200

    added real unit test project

---
+++ RockPaperScissors.UnitTest/Tests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RockPaperScissors.UnitTest
+{
+    public class Tests
+    {
+        [Fact]
+        public void RockBluntsScissors()
+        {
+            var result = new Round().Play("Rock", "Scissors");
+            result.Should().Be(1);
+        }
+    }
+}
