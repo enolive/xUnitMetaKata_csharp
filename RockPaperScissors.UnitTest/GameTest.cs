219c73f2c69e076451d8d82983dd4da7e4fcad60
Christoph Welcz <christoph@welcz.de>
2018-04-01 14:03:50 +0200

    implemented all game tests

--- RockPaperScissors.UnitTest/GameTest.cs
+++ RockPaperScissors.UnitTest/GameTest.cs
@@ -5,16 +5,44 @@ namespace RockPaperScissors.UnitTest
 {
     public class GameTest
     {
+        private readonly SpyGameListener listener;
+        private readonly Game game;
+
+        public GameTest()
+        {
+            listener = new SpyGameListener();
+            game = new Game(listener);
+        }
+
         [Fact]
         public void Player1ShouldWin()
         {
-            var listener = new SpyGameListener();
-            var game = new Game(listener);
             game.PlayRound("Rock", "Scissors");
             game.PlayRound("Rock", "Scissors");
+            listener.Winner.Should().Be(1);
+        }
 
-            var result = listener.Winner;
-            result.Should().Be(1);
+        [Fact]
+        public void Player2ShouldWin()
+        {
+            game.PlayRound("Rock", "Paper");
+            game.PlayRound("Rock", "Paper");
+            listener.Winner.Should().Be(2);
+        }
+
+        [Fact]
+        public void DrawersShouldNotBeCounted()
+        {
+            game.PlayRound("Rock", "Rock");
+            game.PlayRound("Rock", "Rock");
+            listener.Winner.Should().Be(0);
+        }
+
+        [Fact]
+        public void OneRoundShouldNotResultAWinner()
+        {
+            game.PlayRound("Rock", "Paper");
+            listener.Winner.Should().Be(0);
         }
 
         private class SpyGameListener : IGameListener
