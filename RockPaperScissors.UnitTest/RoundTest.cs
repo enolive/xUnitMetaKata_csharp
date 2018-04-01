6cddcec818dbf9149d4285ca55e17c632422f76a
Christoph Welcz <christoph@welcz.de>
2018-04-01 13:43:39 +0200

    extracted round

--- RockPaperScissors.UnitTest/RoundTest.cs
+++ RockPaperScissors.UnitTest/RoundTest.cs
@@ -5,33 +5,45 @@ namespace RockPaperScissors.UnitTest
 {
     public class RoundTest
     {
-        [Fact]
-        public void RockShouldBluntScissors()
+        public RoundTest()
         {
-            new Round().Play("Rock", "Scissors").Should().Be(1);
-            new Round().Play("Scissors", "Rock").Should().Be(2);
+            round = new Round();
         }
 
+        private readonly Round round;
+
         [Fact]
-        public void ScissorsShouldCutPaper()
+        public void InvalidInputShouldMakeRoundFail()
         {
-            new Round().Play("Scissors", "Paper").Should().Be(1);
-            new Round().Play("Paper", "Scissors").Should().Be(2);
         }
 
         [Fact]
         public void PaperShouldWrapRock()
         {
-            new Round().Play("Paper", "Rock").Should().Be(1);
-            new Round().Play("Rock", "Paper").Should().Be(2);
+            round.Play("Paper", "Rock").Should().Be(1);
+            round.Play("Rock", "Paper").Should().Be(2);
+        }
+
+        [Fact]
+        public void RockShouldBluntScissors()
+        {
+            round.Play("Rock", "Scissors").Should().Be(1);
+            round.Play("Scissors", "Rock").Should().Be(2);
         }
 
         [Fact]
         public void SameChoiceShouldResultInDraw()
         {
-            new Round().Play("Rock", "Rock").Should().Be(0);
-            new Round().Play("Paper", "Paper").Should().Be(0);
-            new Round().Play("Scissors", "Scissors").Should().Be(0);
+            round.Play("Rock", "Rock").Should().Be(0);
+            round.Play("Paper", "Paper").Should().Be(0);
+            round.Play("Scissors", "Scissors").Should().Be(0);
+        }
+
+        [Fact]
+        public void ScissorsShouldCutPaper()
+        {
+            round.Play("Scissors", "Paper").Should().Be(1);
+            round.Play("Paper", "Scissors").Should().Be(2);
         }
     }
 }
