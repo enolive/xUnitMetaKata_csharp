4a58b9c912b3af2c6e5a8fbc4c290d8cc7011a2b
Christoph Welcz <christoph@welcz.de>
2018-04-01 15:05:04 +0200

    extracted explaining functions

--- RockPaperScissors/Round.cs
+++ RockPaperScissors/Round.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace RockPaperScissors
@@ -15,24 +14,33 @@ namespace RockPaperScissors
 
         public PlayResult Play(string player1, string player2)
         {
-            if (winning.All(w => w.Item1 != player1))
+            if (IsInvalid(player1) || IsInvalid(player2))
             {
                 throw new InvalidMoveException();
             }
-            if (winning.All(w => w.Item1 != player2))
-            {
-                throw new InvalidMoveException();
-            }
-            if (winning.Any(w => player1 == w.Item1 && player2 == w.Item2))
+
+            if (IsWin(player1, player2))
             {
                 return PlayResult.Player1;
             }
-            if (winning.Any(w => player2 == w.Item1 && player1 == w.Item2))
+
+            if (IsWin(player2, player1))
             {
                 return PlayResult.Player2;
             }
 
             return PlayResult.Draw;
+        }
+
+        private bool IsInvalid(string player)
+        {
+            return winning.All(w => w.Item1 != player);
+        }
+
+        private bool IsWin(string player1, string player2)
+        {
+            var tuple = (player1, player2);
+            return winning.Any(w => w.Equals(tuple));
         }
     }
 }
