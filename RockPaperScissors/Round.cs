3a8a41d5c0f2c1abdea8345f83c16eec41148dc1
Christoph Welcz <christoph@welcz.de>
2018-04-01 16:22:19 +0200

    replaced ifs with a rule chain

--- RockPaperScissors/Round.cs
+++ RockPaperScissors/Round.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -10,6 +11,7 @@ namespace RockPaperScissors
     public class Round
     {
         private readonly IEnumerable<(string, string)> winning;
+        private readonly IEnumerable<Rule> rules;
 
         public Round()
         {
@@ -17,32 +19,49 @@ namespace RockPaperScissors
                 .GetCustomAttributes<WinningAttribute>()
                 .Select(attribute => (attribute.Player1, attribute.Player2))
                 .ToArray();
+            rules = new[]
+            {
+                new Rule(IsInvalid, () => throw new InvalidMoveException()),
+                new Rule(WinsPlayer1, PlayResult.Player1),
+                new Rule(WinsPlayer2, PlayResult.Player2),
+                new Rule(Otherwise, PlayResult.Draw)
+            };
         }
 
-        public PlayResult Play(string player1, string player2)
-        {
-            if (IsInvalid(player1) || IsInvalid(player2))
-            {
-                throw new InvalidMoveException();
-            }
+        public PlayResult Play(string player1, string player2) => rules
+            .Where(rule => rule.IsFullfilledFor(player1, player2))
+            .Select(c => c.Result)
+            .First();
 
-            if (IsWin(player1, player2))
-            {
-                return PlayResult.Player1;
-            }
+        private static bool Otherwise(string player1, string player2) => true;
 
-            if (IsWin(player2, player1))
-            {
-                return PlayResult.Player2;
-            }
+        private bool IsInvalid(string player1, string player2) => IsInvalid(player1) || IsInvalid(player2);
 
-            return PlayResult.Draw;
-        }
+        private bool WinsPlayer2(string player1, string player2) => WinsPlayer1(player2, player1);
 
         private bool IsInvalid(string player) =>
             winning.All(w => w.Item1 != player);
 
-        private bool IsWin(string player1, string player2) =>
+        private bool WinsPlayer1(string player1, string player2) =>
             winning.Any(w => w.Equals((player1, player2)));
+
+        private class Rule
+        {
+            private readonly Func<PlayResult> result;
+            public Func<string, string, bool> IsFullfilledFor { get; }
+
+            public PlayResult Result => result();
+
+            public Rule(Func<string, string, bool> condition, Func<PlayResult> result)
+            {
+                this.result = result;
+                IsFullfilledFor = condition;
+            }
+
+            public Rule(Func<string, string, bool> condition, PlayResult result)
+                : this(condition, () => result)
+            {
+            }
+        }
     }
 }
