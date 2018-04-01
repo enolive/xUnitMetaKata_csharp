afcfcff4a08daab1fa2b42171f8c3be70fed5399
Christoph Welcz <christoph@welcz.de>
2018-04-01 13:37:23 +0200

    implemented scissors/paper and paper/rock

--- RockPaperScissors.UnitTest/Tests.cs
+++ RockPaperScissors.UnitTest/Tests.cs
@@ -6,10 +6,24 @@ namespace RockPaperScissors.UnitTest
     public class Tests
     {
         [Fact]
-        public void RockBluntsScissors()
+        public void RockShouldBluntScissors()
         {
             new Round().Play("Rock", "Scissors").Should().Be(1);
             new Round().Play("Scissors", "Rock").Should().Be(2);
+        }
+
+        [Fact]
+        public void ScissorsShouldCutPaper()
+        {
+            new Round().Play("Scissors", "Paper").Should().Be(1);
+            new Round().Play("Paper", "Scissors").Should().Be(2);
+        }
+
+        [Fact]
+        public void PaperShouldWrapRock()
+        {
+            new Round().Play("Paper", "Rock").Should().Be(1);
+            new Round().Play("Rock", "Paper").Should().Be(2);
         }
     }
 }
