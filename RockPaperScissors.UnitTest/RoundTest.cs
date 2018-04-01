7091b3d92a6e53943d51dcfb03c2feb69faadab1
Christoph Welcz <christoph@welcz.de>
2018-04-01 13:39:58 +0200

    renamed tests for round

--- RockPaperScissors.UnitTest/RoundTest.cs
+++ RockPaperScissors.UnitTest/RoundTest.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace RockPaperScissors.UnitTest
 {
-    public class Tests
+    public class RoundTest
     {
         [Fact]
         public void RockShouldBluntScissors()
@@ -24,6 +24,14 @@ namespace RockPaperScissors.UnitTest
         {
             new Round().Play("Paper", "Rock").Should().Be(1);
             new Round().Play("Rock", "Paper").Should().Be(2);
+        }
+
+        [Fact]
+        public void SameChoiceShouldResultInDraw()
+        {
+            new Round().Play("Rock", "Rock").Should().Be(0);
+            new Round().Play("Paper", "Paper").Should().Be(0);
+            new Round().Play("Scissors", "Scissors").Should().Be(0);
         }
     }
 }
