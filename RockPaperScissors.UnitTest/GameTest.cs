4f21547afce212af6835da27e2098e9803665046
Christoph Welcz <christoph@welcz.de>
2018-04-01 13:51:11 +0200

    implemented first game test

---
+++ RockPaperScissors.UnitTest/GameTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RockPaperScissors.UnitTest
+{
+    public class GameTest
+    {
+        [Fact]
+        public void Player1ShouldWin()
+        {
+            var listener = new SpyGameListener();
+            var game = new Game(listener);
+            game.PlayRound("Rock", "Scissors");
+            game.PlayRound("Rock", "Scissors");
+
+            var result = listener.Winner;
+            result.Should().Be(1);
+        }
+
+        private class SpyGameListener : IGameListener
+        {
+            public void GameOver(int winner)
+            {
+                Winner = winner;
+            }
+
+            public int Winner { get; private set; }
+        }
+    }
+}
