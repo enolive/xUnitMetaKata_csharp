16d8af94b7e0f267b7312f3454849abc33bc7625
Christoph Welcz <christoph@welcz.de>
2018-04-02 17:45:38 +0200

    created golden master

---
+++ RockPaperScissors.MetaTest/GoldenMasterTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using RockPaperScissors.Test;
+using Xunit;
+
+namespace RockPaperScissors.MetaTest
+{
+    public class GoldenMasterTest
+    {
+        private const string ExpectedOutput = @"Running RockPaperScissors tests...
+Round tests...
+rock blunts scissors (Rock, Scissors): PASS
+rock blunts scissors (Scissors, Rock): PASS
+scissors cut paper (Scissors, Paper): PASS
+scissors cut paper (Paper, Scissors): PASS
+paper wraps rock (Paper, Rock): PASS
+paper wraps rock (Rock, Paper): PASS
+round is a draw (Rock, Rock): PASS
+round is a draw (Scissors, Scissors): PASS
+round is a draw (Paper, Paper): PASS
+invalid inputs not allowed: PASS
+Game tests...
+player 1 wins game: PASS
+player 2 wins game: PASS
+drawers not counted: PASS
+invalid moves not counted: PASS
+Tests run: 14  Passed: 14  Failed: 0
+";
+
+        [Fact]
+        public void TestRunShouldHaveExpectedOutput()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                Program.Main(SomeArgs());
+                OutputFrom(writer).Should().Be(ExpectedOutput);
+            }
+        }
+
+        private static string OutputFrom(StringWriter writer) => writer.GetStringBuilder().ToString();
+
+        private static string[] SomeArgs() => Enumerable.Empty<string>().ToArray();
+    }
+}
--- RockPaperScissors.Test/Program.cs
+++ RockPaperScissors.Test/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace RockPaperScissors.Test
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int testsPassed = 0;
             int testsFailed = 0;
